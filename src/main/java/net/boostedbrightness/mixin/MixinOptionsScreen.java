8d82e0b77267f0880705231826f7624e7c7c9b95
Sarocesch <Sarocesch@outlook.de>
2025-10-25 02:09:11 +0200

    Removed Outdated stuff

--- src/main/java/net/boostedbrightness/mixin/MixinOptionsScreen.java
+++ src/main/java/net/boostedbrightness/mixin/MixinOptionsScreen.java
@@ -4,19 +4,11 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoOptionsScreen.class)
 public abstract class MixinOptionsScreen extends Screen {
 
     protected MixinOptionsScreen(Text title) {
         super(title);
-    }
-
-    @Inject(method = "addOptions", at = @At("RETURN"))
-    private void addBoostedBrightnessSlider(CallbackInfo ci) {
-        // Der Boosted Brightness Slider wurde entfernt, da er nicht mehr benötigt wird
     }
 }
